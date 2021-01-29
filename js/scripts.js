ad7ddbb0633d194598c6511565b62450237fa2b2
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-29 10:03:24 -0800

    Refactor if...else statements to switch cases.

--- js/scripts.js
+++ js/scripts.js
@@ -4,48 +4,46 @@ function Pizza(toppings, size){
   this.size = size;
   }
   pizza1 = new Pizza(["ham", "pineapple"], "medium");
-
   Pizza.prototype.cost = function() {
     let cost = 0
-    if (pizza1.size === "large") {
-      cost = 24;
-      return cost;
-    }
-    else if (pizza1.size === "medium"){
-      cost = 18;
-      return cost;
+    let costSize = pizza1.size; 
+    switch (costSize) {
+      case ("large"):
+        cost = 24;
+        return cost;
+        break;
+      case ("medium"):
+        cost = 17;
+        return cost;
+        break;
+      case ("small"):
+        cost = 12;
+        return cost;
+        break;
+      default:
+        $("div#size").append("Please choose a pizza size")
+    }};
 
-    }
-    else if (pizza1.size === "small"){
-      cost = 12;
-      return cost;
-    }
-    else {
-      return("Please choose a size");
-    }
-  };
-  
-  
-  //   default:
-  //     $("div#size").append("Please choose a size")
-  // }
-  
+
+
+
+
+
   // Pizza.prototype.cost = function() {
   //   let cost = 0
-  //   pizza1.size = costSize;
-  //   const costSize = "large"
-  //   switch (costSize) {
-  //     case ("large"):
-  //       cost = 24;
-  //       return cost;
-  //       break;
-  //     case ("medium"):
-  //       cost = 17;
-  //       return cost;
-  //       break;
-  //     case ("small"):
-  //       cost = 12;
-  //       return cost;
-  //       break;
-  //   }};
-  
+  //   if (pizza1.size === "large") {
+  //     cost = 24;
+  //     return cost;
+  //   }
+  //   else if (pizza1.size === "medium"){
+  //     cost = 18;
+  //     return cost;
+  //   }
+  //   else if (pizza1.size === "small"){
+  //     cost = 12;
+  //     return cost;
+  //   }
+  //   else {
+  //     return("Please choose a pizza size");
+  //   }
+  // };  
